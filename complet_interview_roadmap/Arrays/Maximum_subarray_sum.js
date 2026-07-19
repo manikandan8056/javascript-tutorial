e5104cee4084044bdda917fc26b6c9a7c159d1a4
batman8056 <manimanish8056@gmail.com>
2026-07-19 10:25:50 +0530

    Add maximumSubarraySum function to calculate the maximum sum of contiguous subarray

---
+++ complet_interview_roadmap/Arrays/Maximum_subarray_sum.js
@@ -0,0 +1,21 @@
+
+function maximumSubarraySum(arr){
+    let maxSum = -Infinity;
+
+    for(let i = 0; i < arr.length; i++){
+
+        currentSum = 0;
+
+        for(let j = i; j < arr.length; j++){
+
+            currentSum += arr[j];
+
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+            }
+        }
+ 
+    }
+    return maxSum;
+}
+console.log(maximumSubarraySum([-2,1,-3,4,-1,2,1,-5,4]));
