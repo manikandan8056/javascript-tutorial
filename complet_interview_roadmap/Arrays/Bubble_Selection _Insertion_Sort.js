432c6ffbd26392549bc23423aad48654ec0caf06
batman8056 <manimanish8056@gmail.com>
2026-07-20 18:33:42 +0530

    Rename sortArray to bubbleSortArray and update console log for clarity

--- complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
+++ complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
@@ -1,7 +1,11 @@
 
-function sortArray(arr){
+function bubbleSortArray(arr){
     
 }
 
 
-console.log(sortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+console.log(bubbleSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+
+// console.log(selectionSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+
+// console.log(insertionSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
