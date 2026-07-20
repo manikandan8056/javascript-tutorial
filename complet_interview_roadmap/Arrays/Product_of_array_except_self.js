9afe6852833a40437bff4ea0558f8f268e972119
batman8056 <manimanish8056@gmail.com>
2026-07-20 12:36:49 +0530

    Uncomment productExceptSelf function implementation for clarity

--- complet_interview_roadmap/Arrays/Product_of_array_except_self.js
+++ complet_interview_roadmap/Arrays/Product_of_array_except_self.js
@@ -1,22 +1,22 @@
 // Approach 1: Brute Force
 
-// function productExceptSelf(arr){
-//     let result = []
+function productExceptSelf(arr){
+    let result = []
 
-//     for(let i = 0; i< arr.length; i++){
+    for(let i = 0; i< arr.length; i++){
         
-//         let product = 1;
+        let product = 1;
 
-//         for(let j = 0; j < arr.length; j++){
+        for(let j = 0; j < arr.length; j++){
 
-//             if(i !== j){
-//                 product *=arr[j]
-//             }
-//         }
-//         result.push(product);
-//     }
-//     return result;
-// }
+            if(i !== j){
+                product *=arr[j]
+            }
+        }
+        result.push(product);
+    }
+    return result;
+}
 
 // Approach 2: Prefix & Suffix Product
 
