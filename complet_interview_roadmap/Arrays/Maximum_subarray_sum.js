9a2921096576a8fb54a65a7735fdcd0fd9208937
batman8056 <manimanish8056@gmail.com>
2026-07-20 10:43:26 +0530

    Refactor maximumSubarraySum function to implement Kadane's Algorithm for improved performance

--- complet_interview_roadmap/Arrays/Maximum_subarray_sum.js
+++ complet_interview_roadmap/Arrays/Maximum_subarray_sum.js
@@ -1,23 +1,38 @@
+// function maximumSubarraySum(arr){
+//     let maxSum = -Infinity;
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         let currentSum = 0;
+
+//         for(let j = i; j < arr.length; j++){
+
+//             currentSum += arr[j];
+
+//             if(currentSum > maxSum){
+//                 maxSum = currentSum;
+//             }
+
+//         }
+
+//     }
+    
+//     return maxSum;
+// }
+
+
+// Kadane's Algorithm
 
 function maximumSubarraySum(arr){
-    let maxSum = -Infinity;
+    let currentSum = arr[0];
+    let maxSum = arr[0];
 
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 1; i < arr.length; i++){
+        
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
 
-        let currentSum = 0;
-
-        for(let j = i; j < arr.length; j++){
-
-            currentSum += arr[j];
-
-            if(currentSum > maxSum){
-                maxSum = currentSum;
-            }
-
-        }
-
+        maxSum = Math.max(maxSum , currentSum)
     }
-    
     return maxSum;
 }
 console.log(maximumSubarraySum([-2,1,-3,4,-1,2,1,-5,4]));
