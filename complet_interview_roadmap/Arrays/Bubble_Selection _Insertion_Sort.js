3335a4447d3935dce01b6f180a3c910e48e55c07
batman8056 <manimanish8056@gmail.com>
2026-07-20 23:34:52 +0530

    Rename bubbleSortArray to bubbleSort and update console log for consistency

--- complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
+++ complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
@@ -1,11 +1,27 @@
 
-function bubbleSortArray(arr){
+function bubbleSort(arr){
     
+    for(let i = 0; i < arr.length-1; i++){
+        //  let swapped = false;
+        for(let j = 0; j < arr.length - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                // swapped = true;
+            }
+
+        }
+        // if(!swapped){
+        //     break; 
+        // }
+    }
+    return arr;
 }
 
 
-console.log(bubbleSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+console.log(bubbleSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
 
-// console.log(selectionSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+// console.log(selectionSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
 
-// console.log(insertionSortArray([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+// console.log(insertionSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
