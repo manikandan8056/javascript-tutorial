a8fc4287e8260dde7327cabead5e6c881d909bb3
batman8056 <manimanish8056@gmail.com>
2026-07-21 13:41:50 +0530

    Implement first non-repeating character function and add new file for first repeating character

--- complet_interview_roadmap/Strings/Find_first_non-repeating_character.js
+++ complet_interview_roadmap/Strings/Find_first_non-repeating_character.js
@@ -1,8 +1,34 @@
+// Approach 1
 
+// function firstNonRepeatingCharacter(str){
 
+//     for(let i = 0; i < str.length; i++){
+
+//         let count = 0;
+
+//         for(let j = 0; j < str.length; j++){
+
+//             if(str[i] === str[j]){
+//                 count++;
+//             }
+
+//         }
+
+//         if(count === 1){
+//             return str[i];
+//         }
+
+//     }
+//     return null;
+// }
+
+// Approach 2: Optimized Using an Object (Hash Map)
+function firstNonRepeatingCharacter(str){
+    
+}
 
 
 console.log(firstNonRepeatingCharacter("aabbcdde"));   // c
-console.log(firstNonRepeatingCharacter("javascript")); // j
-console.log(firstNonRepeatingCharacter("aabbcc"));     // null
-console.log(firstNonRepeatingCharacter("swiss"));      // w
+// console.log(firstNonRepeatingCharacter("javascript")); // j
+// console.log(firstNonRepeatingCharacter("aabbcc"));     // null
+// console.log(firstNonRepeatingCharacter("swiss"));      // w
