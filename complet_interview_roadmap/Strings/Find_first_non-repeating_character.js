f939bb254dc144690964bb8542ef95a3d9c4176d
batman8056 <manimanish8056@gmail.com>
2026-07-21 12:10:09 +0530

    Refactor and remove old code snippets; add new implementations for various algorithms including max of three numbers, FizzBuzz, demerit points, odd/even check, count truthy values, string properties, sum of multiples, and grade calculation. Introduce new files for prime number checking, star pattern generation, and first non-repeating character finder.

---
+++ complet_interview_roadmap/Strings/Find_first_non-repeating_character.js
@@ -0,0 +1,8 @@
+
+
+
+
+console.log(firstNonRepeatingCharacter("aabbcdde"));   // c
+console.log(firstNonRepeatingCharacter("javascript")); // j
+console.log(firstNonRepeatingCharacter("aabbcc"));     // null
+console.log(firstNonRepeatingCharacter("swiss"));      // w
