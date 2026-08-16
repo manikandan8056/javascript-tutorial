1218759f7701eed0087769f3c5c01303aa0e40e0
batman8056 <manimanish8056@gmail.com>
2026-08-16 19:31:30 +0530

    Add implementations for bubble sort, selection sort, and insertion sort algorithms with examples and explanations

--- complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
+++ complet_interview_roadmap/Arrays/Bubble_Selection _Insertion_Sort.js
@@ -1,27 +1,55 @@
 
-function bubbleSort(arr){
-    
-    for(let i = 0; i < arr.length-1; i++){
-        //  let swapped = false;
-        for(let j = 0; j < arr.length - i - 1; j++){
-            if(arr[j] > arr[j+1]){
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                // swapped = true;
-            }
+// function bubbleSort(arr){  
+//     for(let i = 0; i < arr.length-1; i++){
+//         //  let swapped = false;
+//         for(let j = 0; j < arr.length - i - 1; j++){
+//             if(arr[j] > arr[j+1]){
+//                 let temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//                 // swapped = true;
+//             }
+//         }
+//         // if(!swapped){
+//         //     break; 
+//         // }
+//     }
+//     return arr;
+// }
 
+
+// console.log(bubbleSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+
+
+// function selectionSort(arr){
+//     for(let i = 0; i < arr.length-1; i++){
+//         let minIndex = i;
+//         for(let j = i+1; j < arr.length; j++){
+//             if(arr[j] < arr[minIndex]){
+//                 minIndex = j
+//             }
+//         }
+//         if(minIndex !== i){
+//             let swap = arr[i];
+//             arr[i] = arr[minIndex];
+//             arr[minIndex] = swap
+//         }
+//     }
+//     return arr;
+// }
+// console.log(selectionSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+
+function insertionSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        let currentValue = arr[i];
+        let j = i - 1;
+        while(j >= 0 && arr[j] > currentValue){
+            arr[j+1] = arr[j]
+            j--;
         }
-        // if(!swapped){
-        //     break; 
-        // }
+        arr[j+1]=currentValue;
     }
     return arr;
 }
 
-
-console.log(bubbleSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
-
-// console.log(selectionSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
-
-// console.log(insertionSort([5, 2, 9, 1, 5, 6]));  // Output: [1, 2, 5, 5, 6, 9]
+console.log(insertionSort([5,2,9,1,5,6]));// Output: [1, 2, 5, 5, 6, 9]
